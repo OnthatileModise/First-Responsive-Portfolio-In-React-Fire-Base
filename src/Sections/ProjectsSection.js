4ecab81c5364526477ea01d00c62ec6d3ce4eee5
CAMPUS\Onthatile.Modise <onthatilem@interfile.co.za>
2024-05-09 16:22:03 +0200

    Carousel Removed

--- src/Sections/ProjectsSection.js
+++ src/Sections/ProjectsSection.js
@@ -3,7 +3,7 @@ import oca from "../images/oca.png"
 import git from "../images/git.png"
 import a from "../images/A+.png"
 import n from "../images/N+.png"
-import { Carousel } from 'react-responsive-carousel';
+// import { Carousel } from 'react-responsive-carousel';
 
 
 export default function ProjectsSection() {
@@ -12,47 +12,47 @@ export default function ProjectsSection() {
             <div className="max-width">
                 <h2 className="title">My Projects and Certifications</h2>
                 <div className="carousel owl-carousel">
-                    <Carousel autoPlay={true} infiniteLoop={true} showStatus={false}>
-                        <div className="card">
-                            <div className="box">
-                                <img src={mrsPatBakery} alt=""/>
-                                <div className="text">Mrs Pat Bakery</div>
-                                <p>Class Project in which i was responsible for the Data Access Layer.</p>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="box">
-                                <img src={n} alt=""/>
-                                <div className="text">N+ Certification</div>
-                                <p>Certificate in Networking from CompTIA.</p>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="box">
-                                <img src={a} alt=""/>
-                                <div className="text">A+ Certification</div>
-                                <p>Certificate in A+ Core1 and Core2 from CompTIA</p>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="box">
-                                <img src={oca} alt=""/>
-                                <div className="text">Java SE8 Programmer |</div>
-                                <p>An Oracle Certified Associate, Java SE 8 Programmer has demonstrated knowledge of
-                                    object-oriented concepts, the Java programming language and general knowledge of
-                                    Java platforms and technologies</p>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="box">
-                                <img src={git} alt=""/>
-                                <a href="https://github.com/OnthatileModise/">
-                                    <div className="text">Git Repository</div>
-                                </a>
-                                <p>Take a look at all the projects i have embarked on so far!</p>
-                            </div>
-                        </div>
-                    </Carousel>
+                    {/*<Carousel autoPlay={true} infiniteLoop={true} showStatus={false}>*/}
+                    {/*    <div className="card">*/}
+                    {/*        <div className="box">*/}
+                    {/*            <img src={mrsPatBakery} alt=""/>*/}
+                    {/*            <div className="text">Mrs Pat Bakery</div>*/}
+                    {/*            <p>Class Project in which i was responsible for the Data Access Layer.</p>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*    <div className="card">*/}
+                    {/*        <div className="box">*/}
+                    {/*            <img src={n} alt=""/>*/}
+                    {/*            <div className="text">N+ Certification</div>*/}
+                    {/*            <p>Certificate in Networking from CompTIA.</p>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*    <div className="card">*/}
+                    {/*        <div className="box">*/}
+                    {/*            <img src={a} alt=""/>*/}
+                    {/*            <div className="text">A+ Certification</div>*/}
+                    {/*            <p>Certificate in A+ Core1 and Core2 from CompTIA</p>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*    <div className="card">*/}
+                    {/*        <div className="box">*/}
+                    {/*            <img src={oca} alt=""/>*/}
+                    {/*            <div className="text">Java SE8 Programmer |</div>*/}
+                    {/*            <p>An Oracle Certified Associate, Java SE 8 Programmer has demonstrated knowledge of*/}
+                    {/*                object-oriented concepts, the Java programming language and general knowledge of*/}
+                    {/*                Java platforms and technologies</p>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*    <div className="card">*/}
+                    {/*        <div className="box">*/}
+                    {/*            <img src={git} alt=""/>*/}
+                    {/*            <a href="https://github.com/OnthatileModise/">*/}
+                    {/*                <div className="text">Git Repository</div>*/}
+                    {/*            </a>*/}
+                    {/*            <p>Take a look at all the projects i have embarked on so far!</p>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</Carousel>*/}
                 </div>
             </div>
         </section>
